2c70edd479c923743ddd717623bfc63f68b8ffe2
Mark Estrada <h.mark.estrada@gmail.com>
2022-06-05 17:10:59 -0500

    initial starter upload

--- models/index.js
+++ models/index.js
@@ -1,26 +1,25 @@
-// import models
 const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Products 
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
 });
 
-// Categories have many Products
+// Categories 
 Category.hasMany(Product, {
   foreignKey: 'category_id',
 });
 
-// Products belongToMany Tags (through ProductTag)
+// Products belong To Many 
 Product.belongsToMany(Tag, {
   through: ProductTag,
   foreignKey: 'product_id',
 });
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belong T oMany Products 
 Tag.belongsToMany(Product, {
   through: ProductTag,
   foreignKey: 'tag_id',
